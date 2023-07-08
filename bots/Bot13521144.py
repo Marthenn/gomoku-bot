a5d031df1f83a76cd51eec37838b0b8a9456ba2d
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-08 22:32:32 +0700

    feat: value to coordinate

--- bots/Bot13521144.py
+++ bots/Bot13521144.py
@@ -12,6 +12,19 @@ class Bot13521144(object):
 
         self.NIM = "13521144"
 
+        self.number = -1
+
+        self.board_score = [
+            [0, 0, 0, 0, 0, 0, 0, 0],  # 0
+            [0, 0, 0, 0, 0, 0, 0, 0],  # 1
+            [0, 0, 0, 0, 0, 0, 0, 0],  # 2
+            [0, 0, 0, 1024, 1024, 0, 0, 0],  # 3
+            [0, 0, 0, 0, 0, 0, 0, 0],  # 4
+            [0, 0, 0, 0, 0, 0, 0, 0],  # 5
+            [0, 0, 0, 0, 0, 0, 0, 0],  # 6
+            [0, 0, 0, 0, 0, 0, 0, 0]  # 7
+        ]
+
     def set_player_ind(self, p):
         self.player = p
 
@@ -40,6 +53,37 @@ class Bot13521144(object):
     def __str__(self):
         return "{} a.k.a Player {}".format(self.NIM,self.player)
     
+    def value_to_coordinate(self, value : int) -> tuple:
+        print(value)
+        x = value // 8
+        y = value % 8
+        return (x, y)
+    
+    def update_board_score(self, board : Board):
+        # update all blocked cells value to -1
+        for cells in board.states:
+            print(cells)
+
+        # check if there's any 5 in a row that are not blocked
+        # the 5 in a row can be horizontal, vertical, or diagonal
+        # if there's any, then the score of the cell is 1024
+        print(self.board_score)
+    
+    def get_best_move(self):
+        # max_val = -10000
+        # max_x = -1
+        # max_y = -1
+        # for i in range(8):
+        #     for j in range(8):
+        #         if self.board_score[i][j] > max_val:
+        #             max_val = self.board_score[i][j]
+        #             max_x = i
+        #             max_y = j
+        # return max_x, max_y
+        x = random.randint(0,7)
+        y = random.randint(0,7)
+        return x, y
+
     def get_input(self, board : Board) -> str:
         """
             Parameter board merepresentasikan papan permainan. Objek board memiliki beberapa
@@ -64,6 +108,19 @@ class Bot13521144(object):
 
             TODO: Tentukan x,y secara greedy. Kembalian adalah sebuah string "x,y"
         """
-        x = random.randint(1, board.height)
-        y = random.randint(1, board.width)
+        # update_board_score(board)
+        # x, y = get_best_move(board)
+        for cells in board.states:
+            a, b = self.value_to_coordinate(cells)
+            print(a, b)
+        x = random.randint(0,7)
+        y = random.randint(0,7)
         return f"{x},{y}"
+
+# kalau udah tengah, ambil diagonal kiri atas
+# ambil yang bawah kiri abis itu
+# potong tengah bawah
+# potong tengah kiri
+# potong jangan sampai memanjang 
+# kalau ada tiga potong
+# kalau ada empat halang
