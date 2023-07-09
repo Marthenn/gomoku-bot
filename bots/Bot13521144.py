80ee5f3569515b4484f208e862401ca9f6db00f8
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-09 13:54:39 +0700

    feat: get longest

--- bots/Bot13521144.py
+++ bots/Bot13521144.py
@@ -12,19 +12,6 @@ class Bot13521144(object):
 
         self.NIM = "13521144"
 
-        self.board_state = {}
-
-        self.board_score = [
-            [0, 0, 0, 0, 0, 0, 0, 0],  # 0
-            [0, 0, 0, 0, 0, 0, 0, 0],  # 1
-            [0, 0, 0, 0, 0, 0, 0, 0],  # 2
-            [0, 0, 0, 1024, 1024, 0, 0, 0],  # 3
-            [0, 0, 0, 0, 0, 0, 0, 0],  # 4
-            [0, 0, 0, 0, 0, 0, 0, 0],  # 5
-            [0, 0, 0, 0, 0, 0, 0, 0],  # 6
-            [0, 0, 0, 0, 0, 0, 0, 0]  # 7
-        ]
-
     def set_player_ind(self, p):
         self.player = p
 
@@ -57,7 +44,7 @@ class Bot13521144(object):
         print(value)
         x = value // 8
         y = value % 8
-        return (x, y)
+        return x, y
     
     def is_diagonal(self, val1, val2):
         x1, y1 = self.value_to_coordinate(val1)
@@ -74,30 +61,101 @@ class Bot13521144(object):
         x2, y2 = self.value_to_coordinate(val2)
         return y1 == y2
     
-    def update_board_score(self, board : Board):
-        # update all blocked cells value to -1 using board.states
-        # that are not contained in self.board_states
-        diff = set(board.states.keys()) - set(self.board_state.keys())
-        for i in diff:
-            self.board_score[self.value_to_coordinate(i)[0]][self.value_to_coordinate(i)[1]] = -1   
-
-        self.board_state = board.states
-        # check if there's any 5 in a row that are not blocked
-        # the 5 in a row can be horizontal, vertical, or diagonal
-        # if there's any, then the score of the cell is 1024 
+    def get_adjacents(self, board, value):
+        diag = []
+        hor = []
+        ver = []
+        for x in board.states:
+            if x == value:
+                if self.is_diagonal(x, value):
+                    diag.append(x)
+                if self.is_horizontal(x, value):
+                    hor.append(x)
+                if self.is_vertical(x, value):
+                    ver.append(x)
+        return diag, hor, ver
     
-    def get_best_move(self):
-        # max_val = -10000
-        # max_x = -1
-        # max_y = -1
-        # for i in range(8):
-        #     for j in range(8):
-        #         if self.board_score[i][j] > max_val:
-        #             max_val = self.board_score[i][j]
-        #             max_x = i
-        #             max_y = j
-        # return max_x, max_y
-        return random.randint(0, 7), random.randint(0, 7)
+    def not_blocked(self, board, adj, val, type):
+        # sort adj list
+        adj.sort()
+        start = adj[0]
+        end = adj[-1]
+        a = None # left or up
+        b = None # right or down
+        if type == 0:
+            # check whether it's a / or \ diagonal
+            _, start_y = self.value_to_coordinate(start)
+            _, end_y = self.value_to_coordinate(end)
+            if start_y > end_y:
+                # it's a /, check for the bottom left and top right
+                start_x, start_y = self.value_to_coordinate(start)
+                end_x, end_y = self.value_to_coordinate(end)
+                if start_x != 0 and start_y != 0:
+                    if start-9 not in board.states:
+                        a = start-9
+                if end_x != 7 and end_y != 7:
+                    if end+9 not in board.states:
+                        b = end+9
+            else:
+                # it's a \, check for the top left and bottom right
+                start_x, start_y = self.value_to_coordinate(start)
+                end_x, end_y = self.value_to_coordinate(end)
+                if start_x != 0 and start_y != 7:
+                    if start-7 not in board.states:
+                        a = start-7
+                if end_x != 7 and end_y != 0:
+                    if end+7 not in board.states:
+                        b = end+7
+        elif type == 1:
+            # check left and right of the horizontal
+            _, start_y = self.value_to_coordinate(start)
+            _, end_y = self.value_to_coordinate(end)
+            if start_y != 0:
+                if start-1 not in board.states:
+                    a = start-1
+            if end_y != 7:
+                if end+1 not in board.states:
+                    b = end+1
+        else:
+            # check up and down of the vertical
+            start_x, _ = self.value_to_coordinate(start)
+            end_x, _ = self.value_to_coordinate(end)
+            if start_x != 0:
+                if start-8 not in board.states:
+                    a = start-8
+            if end_x != 7:
+                if end+8 not in board.states:
+                    b = end+8
+        return a, b
+    
+    def longest_unblocked(self, board, player):
+        enemy = 1 if player == 2 else 2
+        val = self.player if player else enemy
+        max_len = 0
+        adj = []
+        type = -1 # 0 = diag, 1 = hor, 2 = ver
+        for x in board.states:
+            if x == val:
+                diag, hor, ver = self.get_adjacents(board, x)
+                if len(diag) > max_len:
+                    a, b = self.not_blocked(board, diag, val, 0)
+                    if a is not None or b is not None:
+                        max_len = len(diag)
+                        adj = diag
+                        type = 0
+                if len(hor) > max_len:
+                    a, b = self.not_blocked(board, hor, val, 1)
+                    if a is not None or b is not None:
+                        max_len = len(hor)
+                        adj = hor
+                        type = 1
+                if len(ver) > max_len:
+                    a, b = self.not_blocked(board, ver, val, 2)
+                    if a is not None or b is not None:
+                        max_len = len(ver)
+                        adj = ver
+                        type = 2
+        return adj, a, b, type
 
     def get_input(self, board : Board) -> str:
         """
@@ -123,8 +181,8 @@ class Bot13521144(object):
 
             TODO: Tentukan x,y secara greedy. Kembalian adalah sebuah string "x,y"
         """
-        # self.update_board_score(board)
-        x, y = self.get_best_move()
+        if board.states == {}:
+            return "3,3" # kalau kosong, ambil tengah
         return f"{x},{y}"
 
 # kalau udah tengah, ambil diagonal kiri atas
